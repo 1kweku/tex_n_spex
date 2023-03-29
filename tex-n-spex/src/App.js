14fa7a52dffb6e5366daa246c32fed3a3f254af5
Bouraima Diarra <84052714+bouraima7@users.noreply.github.com>
2023-03-29 04:27:21 -0400

    g

--- tex-n-spex/src/App.js
+++ tex-n-spex/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Website from "./components/website";
+import "./components/css/App.css";
 // import portfolio from "./components/portfolio";
 
 const App = () => <Website />;
--- tex-n-spex/src/components/navBar.js
+++ tex-n-spex/src/components/navBar.js
@@ -1,5 +1,6 @@
 import React from "react";
-import logo from "./pictures/TNS_3.png";
+import logo from "./pictures/TNS_2.png";
+import "./css/App.css";
 // import Registration from "./registration";
 
 export default function Navbar(props) {
@@ -7,14 +8,23 @@ export default function Navbar(props) {
     <nav className="nav" 
       style= {{
         backgroundColor: "grey",
-        textAlign: "center",
+        textAlign: "left",
+        display : "block",
         marginRight: 0,
         marginLeft: "auto",
         border: "1px solid red",
       }}
     >
 
-      <img src={logo} className="logo" height="200px" width="200px" alt="Tex N Spex Logo" />
+      <img src={logo} className="logo" height="150px" width="150px" alt="Tex N Spex Logo" style={{
+        borderBottomRightRadius: 100,
+        borderBottomLeftRadius: 100,
+        borderTopRightRadius: 100,
+        borderTopLeftRadius: 100,
+        position: "relative",
+        left: 30,
+        right: 40,
+      }} />
       <ul>
         <ol className="nav-item" onClick={(e) => props.pageHandler("Home")}>
           <a href="#Home"> Home </a>
