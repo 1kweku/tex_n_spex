7a0dc94aa16d1c51c607e8cf3db03eb368adde74
Bouraima Diarra <84052714+bouraima7@users.noreply.github.com>
2023-03-29 03:11:13 -0400

    commit

--- tex-n-spex/src/components/pages/home.js
+++ tex-n-spex/src/components/pages/home.js
@@ -1,6 +1,9 @@
+import logo from '../pictures/TNS_2.png';
+
 export default function Home() {
   return (
     <div>
+      <img src={logo} className="logo" height="200px" width="200px" alt="Tex N Spex Logo" />
       <p> "Home"</p>
     </div>
   );
