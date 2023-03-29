eab62aa3262da93fc64e814ce7d81448bf927be2
Bouraima Diarra <84052714+bouraima7@users.noreply.github.com>
2023-03-29 03:05:03 -0400

    Navbar update and picture upload

--- tex-n-spex/src/components/navBar.js
+++ tex-n-spex/src/components/navBar.js
@@ -1,9 +1,20 @@
 import React from "react";
+import logo from "./pictures/TNS_3.png";
 // import Registration from "./registration";
 
 export default function Navbar(props) {
   return (
-    <nav className="nav">
+    <nav className="nav" 
+      style= {{
+        backgroundColor: "grey",
+        textAlign: "center",
+        marginRight: 0,
+        marginLeft: "auto",
+        border: "1px solid red",
+      }}
+    >
+
+      <img src={logo} className="logo" height="200px" width="200px" alt="Tex N Spex Logo" />
       <ul>
         <ol className="nav-item" onClick={(e) => props.pageHandler("Home")}>
           <a href="#Home"> Home </a>
